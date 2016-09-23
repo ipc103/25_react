d41b979af30ebe87bdb1ecc55493bfa05cfd1372
ipc103 <iancandysemail@gmail.com>
2016-09-23 09:56:24 -0400

    remove solution

--- src/components/gif_item.js
+++
@@ -1,9 +0,0 @@
-import React, {Component} from 'react';
-
-export default function GifItem (props) {
-  return (
-    <div>
-      <img className='center-block' src={props.url} />
-    </div>);
-
-}
